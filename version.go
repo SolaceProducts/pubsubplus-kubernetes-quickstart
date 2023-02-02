66b992a3a576422b4dd484882250f929dee67e91
vivek02172003 <79029055+vivek02172003@users.noreply.github.com>
2023-02-02 12:18:30 -0500

    removed - from package name

--- version.go
+++ version.go
@@ -14,6 +14,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package pubsubplus-kubernetes-operator
+package pubsubplusKubernetesOperator
 
 const version = "1.0.0"
